84557c72801a314e1da637715b07f9ba029df256
TheaFWang <691824984@qq.com>
2024-08-15 23:42:27 +0800

    11

--- zym_api/Controllers/LoginController.cs
+++ zym_api/Controllers/LoginController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace zym_api.Controllers
 {
-    public class LoginController
+    public class LoginController : ApiController 
     {
+
     }
 }
