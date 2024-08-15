eddbe60c5e6207adbe141a9fe5c285710143e8c9
TheaFWang <691824984@qq.com>
2024-08-16 00:05:04 +0800

    1212
    12

--- zym_api/Controllers/LoginController.cs
+++ zym_api/Controllers/LoginController.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Http;
+using zym_api.BLL;
+using zym_api.Models;
 
 namespace zym_api.Controllers
 {
     public class LoginController : ApiController 
     {
-
+        [HttpGet]
+        public HttpResponseMessage GetOpenId()
+        {
+            return null;
+        }
     }
 }
